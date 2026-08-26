bfbf6a15d39543b29493349df8995041b60193de
joelcinkenke2009-dotcom <joel.cinkenke2009@gmail.com>
2026-08-27 00:01:03 +0100

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,3 @@
 export const apiUrl = "https://back-end-tracker-yhwf.onrender.com"
-export const metaApp = "1044158895157871"
-export const config_id = "1600413744975143"
+export const metaApp = "1083363634373087"
+export const config_id = "1638883141127442"
